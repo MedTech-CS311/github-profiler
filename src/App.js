4a63c295c77bd83e6846da9e88a3e82a80a5f392
Ikdemm <ikdem.benmbarek@aiesec.net>
2022-01-16 20:01:06 +0100

    added navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 // Components
 import RepositoriesList from './components/repositories/RepositoriesList';
 import Profile from './components/profile/Profile';
+import Navbar from './components/layouts/Navbar';
 
 // eslint-disable-next-line
 import authInterceptor from './interceptors';
@@ -10,6 +11,7 @@ import authInterceptor from './interceptors';
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <div className='app-container'>
         <div className="profile-container">
           <Profile />
---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import logo from "../../assets/img/logo.svg";
+
+export default class Navbar extends React.Component {
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         profile: {}
+    //     }
+    // }
+
+    render() {
+        return (
+            <div className="navbar">
+                <div className="navbar-logo-container">
+                    <img src={logo} className="navbar-logo-img" alt="logo" />
+                </div>
+            </div>
+        )
+    }
+
+} 
