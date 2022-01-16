02559ec8abe3be8700742dbba3811a7901ca0e22
Ikdemm <ikdem.benmbarek@aiesec.net>
2022-01-16 15:39:56 +0100

    profile card init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+
+// Components
 import RepositoriesList from './components/repositories/RepositoriesList';
+import Profile from './components/profile/Profile';
+
 // eslint-disable-next-line
 import authInterceptor from './interceptors';
 
 function App() {
   return (
     <div className="App">
-      <RepositoriesList />
+      <div className='app-container'>
+        <div className="profile-container">
+          <Profile />
+        </div>
+      </div>
     </div>
   );
 }
