b8e9636f612d55c240878b8c50a10dbb850a7ca9
Ikdemm <ikdem.benmbarek@aiesec.net>
2022-01-16 15:51:38 +0100

    fetched and rendered profile data

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -9,14 +10,32 @@ export default class Profile extends React.Component {
     constructor() {
         super()
         this.state = {
+            profile: {}
         }
+    }
+
+    componentDidMount() {
+        axios.get(`https://api.github.com/user`)
+        .then((response) => {
+            this.setState({profile: response.data})
+        })
+    }
+
+    handleNavigateToProfile = () => {
+        window.location = this.state.profile.html_url
     }
 
     render() {
         return (
             <Card>
                 <CardContent>
-                    <h2>profile here</h2>
+                    <img src={this.state.profile.avatar_url} alt="avatar" />
+                    <h3 onClick={this.handleNavigateToProfile}>{this.state.profile.name}</h3>
+                    <p>{this.state.profile.bio}</p>
+                    { this.state.profile.email && <p>Email: {this.state.profile.email}</p>}
+                    <p>Location: {this.state.profile.location}</p>
+                    <p>Company: {this.state.profile.company}</p>
+                    <p>Followers: {this.state.profile.followers}</p>
                 </CardContent>
             </Card>
         )
