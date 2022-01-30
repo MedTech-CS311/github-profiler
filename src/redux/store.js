61b5cc848864f5a40b56ee78329548540d35652c
Ikdemm <ikdem.benmbarek@aiesec.net>
2022-01-30 23:04:17 +0100

    added reposReducer to the root redcuer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import profileReducer from "./profile/profile.reducer";
+import reposReducer from "./repos/repos.reducer";
 
 // Creating the root reducer by combining the reducers
 const rootReducer = combineReducers({
   profile: profileReducer,
+  repos: reposReducer,
 });
 
 // Creating the store with the root reducer
